229f4ac2aca701dccc0cc82dd4ffd16c5207f7b3
PhullCraft <amardeepsinghp@gmail.com>
2024-01-01 14:25:50 +0530

    This is chap 5 practice, present in section 2. this demonsate creation of method only, calling it will be part of the further chapter

---
+++ src/section_2/UsageOfMethodInJava.java
@@ -0,0 +1,19 @@
+package section_2;
+
+public class UsageOfMethodInJava {
+	
+	static int a =10;
+	
+	public void getData() {
+		System.out.println("I am in method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Value of a is "+a);
+
+	}
+
+}
