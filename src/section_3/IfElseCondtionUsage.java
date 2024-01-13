7e504d50f17c01105b125f4af40377d147693ec2
PhullCraft <amardeepsinghp@gmail.com>
2024-01-13 15:20:07 +0530

    This is commit for adding section 3 package, for practicing if else conditon

---
+++ src/section_3/IfElseCondtionUsage.java
@@ -0,0 +1,18 @@
+package section_3;
+
+public class IfElseCondtionUsage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+			
+		if (i ==1) {	
+			System.out.println("i is number 1");
+		}
+		else {
+			System.out.println("i is not number 1");	
+		}
+	}
+
+	
+}
