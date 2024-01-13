8f612d2f313ad22525012ae76c3495bdbb41bb70
PhullCraft <amardeepsinghp@gmail.com>
2024-01-13 17:30:19 +0530

    This is comment to demonstrate how For Loop works, this if form lesson 8, section3

---
+++ src/section_3/HowForLoopWork_8.java
@@ -0,0 +1,18 @@
+package section_3;
+
+public class HowForLoopWork_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for(int i=0; i<=5;i++) { // int i=0; i<=5 this steps will be executed first, but i ++ happens at the end of the loop
+			
+			System.out.println(i);
+			
+			//i++ of the loops is execute a the end of the loop
+		}
+
+	}
+
+}
